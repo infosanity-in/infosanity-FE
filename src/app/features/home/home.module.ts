7b7442d56c69687dcb82d1dcdcc8c971e90617ae
Gaurav Bansal <gauravbansal67@hotmail.com>
2021-05-03 15:07:29 +0530

    feat: added navbar component

--- src/app/features/home/home.module.ts
+++ src/app/features/home/home.module.ts
@@ -2,9 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
-  declarations: [HomeComponent],
-  imports: [CommonModule, HomeRoutingModule],
+  declarations: [HomeComponent, NavbarComponent],
+  imports: [
+    CommonModule,
+    HomeRoutingModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
 })
 export class HomeModule {}
